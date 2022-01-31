d853e1d72770a37fb8b8a546cedeb377a12aad05
McKotter <93869121+Kalondos@users.noreply.github.com>
2022-01-31 13:36:39 +0200

    Add files via upload

---
+++ gr1.py
@@ -0,0 +1,23 @@
+'''
+ Karl Eerik Kotter
+ 31.01.2022
+ Ülesanne 01
+'''
+
+
+
+#väljastan teksti
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+#väljastan konna kujutise
+print("   @..@    ")
+print("  (----)   ")
+print(" ( \__/ )  ")
+print(" ^^ "" ^^  ")
+
+#ruudu pindala valem a*a
+a = int(input("Sisesta ruudu külg a = "))
+s = a*a
+print("vastus: ruudu pindala on ", str(s)+" cm^2")
+
+
