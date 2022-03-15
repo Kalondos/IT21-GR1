bbfcb2e8cd55ea4f02aed451c2ad314e0d6066d8
McKotter <93869121+Kalondos@users.noreply.github.com>
2022-03-15 13:12:24 +0200

    Add files via upload

---
+++ Graafiline UL1.py
@@ -0,0 +1,82 @@
+##############
+# Karlos eeriko Kotter
+# 15.03.2022
+# UL1
+##############
+
+import turtle
+#Aken
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.bgcolor("black")
+
+
+#joon 1 paremale
+tk = turtle.Turtle()
+tk.color("lightgreen") 
+tk.penup()
+tk.left(0)
+tk.pendown()
+tk.forward(100)
+
+#joon 2 vasakule
+tk = turtle.Turtle()
+tk.color("lightblue")
+tk.penup()
+tk.right(180)
+tk.pendown()
+tk.forward(100)
+
+#joon 3 ülesse
+tk = turtle.Turtle()
+tk.color("red")
+tk.penup()
+tk.right(-90)
+tk.pendown()
+tk.forward(100)
+
+#joon 4 alla
+tk = turtle.Turtle()
+tk.color("yellow")
+tk.penup()
+tk.right(90)
+tk.pendown()
+tk.forward(100)
+
+#joon 5 45kraadi ülesse
+tk = turtle.Turtle()
+tk.color("Green")
+tk.penup()
+tk.right(-45)
+tk.pendown()
+tk.forward(100)
+
+#joon 6 45kraadi ülesse
+tk = turtle.Turtle()
+tk.color("orange")
+tk.penup()
+tk.right(45)
+tk.pendown()
+tk.forward(100)
+
+#joon 7 45kraadi ülesse
+tk = turtle.Turtle()
+tk.color("pink")
+tk.penup()
+tk.right(-135)
+tk.pendown()
+tk.forward(100)
+
+#joon 8 45kraadi alla
+tk = turtle.Turtle()
+tk.color("lightpink")
+tk.penup()
+tk.right(135)
+tk.pendown()
+tk.forward(100)
+
+turtle.exitonclick()
+
+
+
+
