edbafca856870a64bec15682467c816dda1faeb8
McKotter <93869121+Kalondos@users.noreply.github.com>
2022-03-24 13:56:42 +0200

    Add files via upload

---
+++ pilt.py
@@ -0,0 +1,31 @@
+
+
+
+'''
+print("\n------------ Vägimees Leiger ------------\n")
+print("Mängu alustamiseks vajutage [Enter] nupule!")
+input()
+
+
+print("Oli kord aeg, mil Hiiumaal ringles ringi üks vägimees.\nKel nimeks olevat Leiger, ta olnud suur ja tugev mees.")
+input()
+'''
+
+
+
+def valik_menuu():
+    print("[1] Tervita ja ärkad voodist")
+    print("[2] Maga edasi. (pead ootama 20 sekundit, edasi mängimiseks.)")
+valik_menuu()
+    
+    
+print("Suur Töll: Tervist vennake. Mis sa magad siin, päev on juba poole peal.\n")
+valik = int(input("Sisestage valik: "))
+
+if valik_menuu == 1:
+    print("Leiger: Tervist, tervist! Mis sind siia siis kah tõi?")
+elif valik_menuu == 2:
+    print("tere")
+    
+input()
+    
